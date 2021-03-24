fac536b4165b6a473ba50eff8776eecd681423a9
Szpabt-zz <stoida146@gmail.com>
2021-03-24 23:44:57 +0200

    com

--- Ex3_MDI_Application/ParentForm.Designer.cs
+++ Ex3_MDI_Application/ParentForm.Designer.cs
@@ -36,7 +36,17 @@
             this.WindowMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.WindowCascadeMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.WindowTileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.spWin = new System.Windows.Forms.ToolStripStatusLabel();
+            this.spData = new System.Windows.Forms.ToolStripStatusLabel();
             this.MdiMenu.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // MdiMenu
@@ -45,7 +55,8 @@
             this.MdiMenu.Location = new System.Drawing.Point(0, 0);
             this.MdiMenu.MdiWindowListItem = this.WindowMenuItem;
             this.MdiMenu.Name = "MdiMenu";
-            this.MdiMenu.Size = new System.Drawing.Size(404, 24);
+            this.MdiMenu.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.MdiMenu.Size = new System.Drawing.Size(432, 28);
             this.MdiMenu.TabIndex = 1;
             this.MdiMenu.Text = "menuStrip1";
             // 
@@ -53,20 +64,20 @@
             // 
             this.FileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.NewMenuItem, this.ExitMenuItem});
             this.FileMenuItem.Name = "FileMenuItem";
-            this.FileMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.FileMenuItem.Size = new System.Drawing.Size(44, 24);
             this.FileMenuItem.Text = "&File";
             // 
             // NewMenuItem
             // 
             this.NewMenuItem.Name = "NewMenuItem";
-            this.NewMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.NewMenuItem.Size = new System.Drawing.Size(108, 24);
             this.NewMenuItem.Text = "&New";
             this.NewMenuItem.Click += new System.EventHandler(this.NewMenuItem_Click);
             // 
             // ExitMenuItem
             // 
             this.ExitMenuItem.Name = "ExitMenuItem";
-            this.ExitMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ExitMenuItem.Size = new System.Drawing.Size(108, 24);
             this.ExitMenuItem.Text = "&Exit";
             this.ExitMenuItem.Click += new System.EventHandler(this.ExitMenuItem_Click);
             // 
@@ -74,37 +85,130 @@
             // 
             this.WindowMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.WindowCascadeMenuItem, this.WindowTileMenuItem});
             this.WindowMenuItem.Name = "WindowMenuItem";
-            this.WindowMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.WindowMenuItem.Size = new System.Drawing.Size(76, 24);
             this.WindowMenuItem.Text = "&Window";
             // 
             // WindowCascadeMenuItem
             // 
             this.WindowCascadeMenuItem.Name = "WindowCascadeMenuItem";
-            this.WindowCascadeMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.WindowCascadeMenuItem.Size = new System.Drawing.Size(133, 24);
             this.WindowCascadeMenuItem.Text = "&Cascade";
             this.WindowCascadeMenuItem.Click += new System.EventHandler(this.WindowCascadeMenuItem_Click);
             // 
             // WindowTileMenuItem
             // 
             this.WindowTileMenuItem.Name = "WindowTileMenuItem";
-            this.WindowTileMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.WindowTileMenuItem.Size = new System.Drawing.Size(133, 24);
             this.WindowTileMenuItem.Text = "&Tile";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.toolStripButton1, this.toolStripSeparator1, this.toolStripButton2, this.toolStripButton3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 28);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(432, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Tag = "";
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::Ex3_MDI_Application.Properties.Resources.TexturesCom_Flames0036_1_S;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Tag = "NewDoc";
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.ToolTipText = "Create new document";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = global::Ex3_MDI_Application.Properties.Resources.NewFile1;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Tag = "Cascade";
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.ToolTipText = "Windows cascade";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = global::Ex3_MDI_Application.Properties.Resources.TexturesCom_Aerials0093_1_S;
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Tag = "Title";
+            this.toolStripButton3.Text = "toolStripButton3";
+            this.toolStripButton3.ToolTipText = "Windows title";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.spWin, this.spData});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 278);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(432, 25);
+            this.statusStrip1.TabIndex = 5;
+            // 
+            // spWin
+            // 
+            this.spWin.Name = "spWin";
+            this.spWin.Size = new System.Drawing.Size(49, 20);
+            this.spWin.Text = "Status";
+            this.spWin.Click += new System.EventHandler(this.spWin_Click);
+            // 
+            // spData
+            // 
+            this.spData.Name = "spData";
+            this.spData.Size = new System.Drawing.Size(41, 20);
+            this.spData.Text = "Data";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(404, 281);
+            this.ClientSize = new System.Drawing.Size(432, 303);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.MdiMenu);
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.MdiMenu;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Parent Form";
             this.MdiMenu.ResumeLayout(false);
             this.MdiMenu.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.ToolStripStatusLabel spData;
+
+        private System.Windows.Forms.ToolStripStatusLabel spWin;
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
 
         private System.Windows.Forms.ToolStripMenuItem WindowCascadeMenuItem;
         private System.Windows.Forms.ToolStripMenuItem WindowMenuItem;
